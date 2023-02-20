f959eb7b888212f0e94dbb953aa6de0364300ed1
pmartinsesa <pmartinsesa@gmail.com>
2023-02-20 12:03:26 -0300

    add comentario

--- src/modules/Student.ts
+++ src/modules/Student.ts
@@ -1,4 +1,4 @@
 export class Student {
   // Criar um sistema de levantamento de dados do aluno.
-  // Com base nesses dados vamos criar algum algoritmo no modelo tutorial.
+  // Com base nesses dados vamos criar algum algoritmo no modelo tutorial para ordenação das dicas.
 }
